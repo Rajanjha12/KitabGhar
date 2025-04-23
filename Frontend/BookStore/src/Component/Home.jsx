e893c684d2e1d72b878325aeb05a3e09e49f5499
Rajanjha12 <rajanjha122004@gmail.com>
2025-04-23 18:14:06 +0530

    update

--- Frontend/BookStore/src/Component/Home.jsx
+++ Frontend/BookStore/src/Component/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Banner from './Banner'
- import Card from './card'
+ import Card from './Card'
 import Footer from './Footer'
 const Home = () => {
   return (
