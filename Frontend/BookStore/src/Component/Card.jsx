b9c60105a9eb6c240f5af5e9a3cee74528017dee
Rajanjha12 <rajanjha122004@gmail.com>
2025-04-23 21:34:04 +0530

    Update

--- Frontend/BookStore/src/Component/Card.jsx
+++ Frontend/BookStore/src/Component/Card.jsx
@@ -15,7 +15,7 @@ const [book,setBook]=useState([])
   useEffect(()=>{
     const getBook=async()=>{
       try{
-        const res=await axios.get("http://localhost:4001/book");
+        const res=await axios.get("https://kitab-ghar-5aef.vercel.app/book");
         console.log(res.data);
         const data=res.data.filter((data)=> data.category === "Free");
         setBook(data);
