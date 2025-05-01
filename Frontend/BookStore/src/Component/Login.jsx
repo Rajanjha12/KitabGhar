2cfe87ca895a333d71fe8601a29e731b1bb50fa9
RAJAN  KUMAR JHA <148378859+Rajanjha12@users.noreply.github.com>
2025-05-01 20:56:27 +0530

    Update Login.jsx

--- Frontend/BookStore/src/Component/Login.jsx
+++ Frontend/BookStore/src/Component/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
       email:data.email,
       password:data.password
      };
-      await axios.post("http://localhost:4001/user/login",userInfo)
+      await axios.post("https://kitab-ghar-5aef.vercel.app/user/login",userInfo)
       //promise is used to handle the response from the server
       .then((res)=>
      {
